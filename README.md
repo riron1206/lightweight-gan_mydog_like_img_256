b1cd8e1002ead1995ab626fc6a5c41c5f1afe182
riron1206 <rironyokoi@gmail.com>
2021-03-01 22:13:10 +0900

    edit

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 - google colab でちまちまやった
 
+- gifは解像度1/4にした版。mp4版はもう少しきれい
+
   
 
 ![demo](generated-03-01-2021_12-42-20_conv2.gif)
--- gif2mp4.py
+++ gif2mp4.py
@@ -14,3 +14,4 @@ clip.close()
 # https://qiita.com/wMETAw/items/fdb754022aec1da88e6e
 #!ffmpeg -i "C:\Users\81908\Pictures\lightweight-gan\mydog_like_img_256\68000step\generated-03-01-2021_12-42-20.mp4" -vf scale=320:-1 -r 10 "C:\Users\81908\Pictures\lightweight-gan\mydog_like_img_256\68000step\generated-03-01-2021_12-42-20_conv.gif"
 #!ffmpeg -i "C:\Users\81908\Pictures\lightweight-gan\mydog_like_img_256\68000step\generated-03-01-2021_12-42-20.mp4" -vf scale=1033:-1 -r 10 "C:\Users\81908\Pictures\lightweight-gan\mydog_like_img_256\68000step\generated-03-01-2021_12-42-20_conv2.gif"
+#!ffmpeg -i "C:\Users\81908\Pictures\lightweight-gan\mydog_like_img_256\68000step\generated-03-01-2021_12-42-20.mp4" -vf scale=566:-1 -r 10 "C:\Users\81908\Pictures\lightweight-gan\mydog_like_img_256\68000step\generated-03-01-2021_12-42-20_conv2.gif"
